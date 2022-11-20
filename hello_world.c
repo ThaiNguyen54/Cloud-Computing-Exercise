cca3448c7d1be091125302f1034bbb92eec21d5c
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2022-11-20 23:21:42 +0700

    added hello_world

---
+++ hello_world.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv []) {
+	int size, rank, namelen;
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &rank);
+
+	MPI_Get_processor_name(processor_name, &namelen);
+
+	printf("Ich bin Prozess Nr. %d von %d auf %s\n", rank, size, processor_name);
+
+	MPI_Finalize();
+
+	return 0;
+}
