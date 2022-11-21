1cad95c460b66b8f3540324fa95ead0c8c49cbfe
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2022-11-22 00:58:12 +0700

    update code

--- hello_world.c
+++ hello_world.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv []) {
 
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	MPI_Comm_size(MPI_COMM_WORLD, &rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	MPI_Get_processor_name(processor_name, &namelen);
 
